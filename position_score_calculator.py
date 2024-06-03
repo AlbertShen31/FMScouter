94411a7f2b6eb74c129e579b488e15ee43ae071d
Albert Shen <albertshen31@gmail.com>
2024-06-03 10:00:40 -0400

    Added main function

--- position_score_calculator.py
+++ position_score_calculator.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python
 import pandas as pd # type: ignore
+import sys
 import glob
 import os
 from typing import List
 import position_config as pc
 from utils import calculate_score, format_position_name, generate_html_multiple, select_best_teams
 from formation import spartans, france, notts_county, short_kings
+from dotenv import load_dotenv
 
-position_lists = [pc.gk_positions, pc.fb_positions, pc.cb_positions, pc.dm_positions, pc.cm_positions, pc.am_positions, pc.w_positions, pc.st_positions]
-
-directory_path = os.environ.get('FM_24_path')
-
-# Find the most recent file in the specified folder
-squad_files = glob.glob(os.path.join(directory_path, '**/*Squad*'), recursive=True)
-squad_file = max(squad_files, key=os.path.getctime)
-
-scouting_files = glob.glob(os.path.join(directory_path, '**/*Scouting*'), recursive=True)
-scouting_file = max(scouting_files, key=os.path.getctime)
-
-# Read HTML file exported by FM - in this case, an example of an output from the squad page
-squad_rawdata_list = pd.read_html(squad_file, header=0, encoding="utf-8", keep_default_na=False)
-scouting_rawdata_list = pd.read_html(scouting_file, header=0, encoding="utf-8", keep_default_na=False)
-
-squad_rawdata = squad_rawdata_list[0]
-scouting_rawdata = scouting_rawdata_list[0]
-
+load_dotenv()
 
 # Calculate the score for each position
-def calculate_positions(rawdata, selected_positions, min_score=0):
+def calculate_positions(rawdata, selected_positions, position_lists, min_score=0):
     # Build a dictionary of selected positions
     position_dict = {pos: attrs for plist in position_lists for pos, attrs in plist.items()
                      if pos.lower() in selected_positions}
@@ -63,15 +48,53 @@ def calculate_positions(rawdata, selected_positions, min_score=0):
 
 
 
-def calculate_formation(formation):
-    squad = calculate_positions(squad_rawdata, formation, min_score=0)
-    scouting = calculate_positions(scouting_rawdata, formation, min_score=0)
+def calculate_formation(formation, squad_rawdata, scouting_rawdata, squad_file, scouting_file):
+    position_lists = [pc.gk_positions, pc.fb_positions, pc.cb_positions, pc.dm_positions, pc.cm_positions, pc.am_positions, pc.w_positions, pc.st_positions]
+    squad = calculate_positions(squad_rawdata, formation, position_lists, min_score=0)
+    scouting = calculate_positions(scouting_rawdata, formation, position_lists, min_score=0)
 
     generate_html_multiple(squad, formation + ["all"], str(os.path.splitext(os.path.basename(squad_file))[0]))
     generate_html_multiple(scouting, formation + ["all"], str(os.path.splitext(os.path.basename(scouting_file))[0]))
 
 
-# calculate_formation(spartans)
-# calculate_formation(france)
-# calculate_formation(notts_county)
-calculate_formation(short_kings)
+
+def main():
+    if len(sys.argv) > 1:
+        user_input = " ".join(sys.argv[1:])
+    else:
+        user_input = ""
+        while not user_input.strip():
+            user_input = input("Please type in the team name: ")
+
+    directory_path = os.getenv('FM_24_path')
+
+    # Find the most recent file in the specified folder
+    squad_files = glob.glob(os.path.join(directory_path, '**/*Squad*'), recursive=True)
+    squad_file = max(squad_files, key=os.path.getctime)
+
+    scouting_files = glob.glob(os.path.join(directory_path, '**/*Scouting*'), recursive=True)
+    scouting_file = max(scouting_files, key=os.path.getctime)
+
+    # Read HTML file exported by FM - in this case, an example of an output from the squad page
+    squad_rawdata_list = pd.read_html(squad_file, header=0, encoding="utf-8", keep_default_na=False)
+    scouting_rawdata_list = pd.read_html(scouting_file, header=0, encoding="utf-8", keep_default_na=False)
+
+    squad_rawdata = squad_rawdata_list[0]
+    scouting_rawdata = scouting_rawdata_list[0]
+
+    if user_input == "spartans":
+        calculate_formation(spartans, squad_rawdata, scouting_rawdata, squad_file, scouting_file)
+        return
+    elif user_input == "france":
+        calculate_formation(france, squad_rawdata, scouting_rawdata, squad_file, scouting_file)
+        return
+    elif user_input == "notts_county":
+        calculate_formation(notts_county, squad_rawdata, scouting_rawdata, squad_file, scouting_file)
+        return
+    elif user_input == "gronigen":
+        calculate_formation(short_kings, squad_rawdata, scouting_rawdata, squad_file, scouting_file)
+        return
+
+if __name__ == "__main__":
+    main()
+
